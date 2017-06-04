7739fe507eed8f6c5354335afb6d38abb207022a
waxdoll23 <jmcgnty23@gmail.com>
2017-06-04 11:52:59 -0400

    Working on getting the api key to work

--- src/main/java/GuardianInfo.java
+++ src/main/java/GuardianInfo.java
@@ -1,8 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by jennifermcginty on 6/2/17.
  */
 public class GuardianInfo {
-    String apiKey = "3dd05513f63849db9ea0f9a1ca9fd482";
+
+     final String apiKey = "3dd05513f63849db9ea0f9a1ca9fd482";
 
 
+    String url = "https://www.bungie.net/en/Legend/Gear/1/4611686018433946292/2305843009217444787";
+    URL obj = new URL(url);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+    con.setRequestMethod("GET");
+    con.setRequestProperty("X-API-Key", apiKey)
 }
